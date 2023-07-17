6c0b1fc6e15fb19edeee6ebc372799632f55eee8
Francisco Pautt <franjpautt@gmail.com>
2023-07-16 23:49:50 -0500

    feat: stack linked list

---
+++ src/stacklinkedlist/Node.java
@@ -0,0 +1,14 @@
+
+package stacklinkedlist;
+
+
+public class Node {
+    public int data;
+    public Node next;
+    
+    //Constructor to get the data when called
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
